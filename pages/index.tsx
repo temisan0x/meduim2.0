85e865446e40fbb404f83eb925169565f826db45
Heytemisan <temyicy@gmail.com>
2022-01-31 23:16:02 +0100

    t
    feat: created header styles and made it very responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,29 @@ import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Meduim 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
+      <Header />
+
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-y-5">
+          <h1 className="text-5xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">
+              Meduim
+            </span> is a place to write, read and connect
+          </h1>
+          <h2>
+            it's easy and free to post your thinking on any topic and connect with millions of readers.
+          </h2>
+        </div>
+
+        <img
+          className="hidden md:inline-flex h-32 lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="meduim logo" />
+      </div>
     </div>
   )
 }
