2d1a20f6942a20d3b37087735ab7f1e22c2de36a
Heytemisan <temyicy@gmail.com>
2022-02-01 21:16:01 +0100

    feat: getStaticPath done

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,3 +1,7 @@
+import Header from "../../components/Header";
+import { sanityClient, urlFor } from '../../sanity';
+import { Post } from "../../typings";
+
 const Post = () => {
     return ( 
         <main>
@@ -9,3 +13,27 @@ const Post = () => {
 export default Post;
 
 //prefetch all the data using getstatic path
+export const getStaticPath = async () => {
+    const query = `*[_type == 'post']{
+        _id,
+        title,
+        slug,
+        
+        author -> {
+        name,
+        image
+    }}`
+
+    const posts = await sanityClient.fetch(query);
+    
+    const paths = posts.map((post: Post) => ({
+        params: {
+            slug: post.slug.current
+        }
+    }));
+
+    return {
+        paths,
+        fallback: 'blocking'
+    }
+}
