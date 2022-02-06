9b95af651691f54a4019c319c0922e8237d6abf9
Heytemisan <temyicy@gmail.com>
2022-02-06 22:17:02 +0100

    feat: created form

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -53,6 +53,31 @@ const Post = ({ post }: Props) => {
                     />
                 </div>
             </article> 
+
+            <hr className="max-w-lg my-5 mx-auto border border-yellow-500"/>
+
+            <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+                <h3 className="text-sm text-yellow-500">Enjoyed the article?</h3>
+                <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+                <hr className="py-3 mt-2" />
+                
+                <label className="block mb-5">
+                    <span className="text-gray-500">Name</span>
+                    <input 
+                        className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 ring-1 outline-none focus:none"
+                        type="text" placeholder="eg Temisan Momodu"/>
+                </label>
+                <label className="block mb-5">
+                    <span className="text-gray-500">Email</span>
+                    <input 
+                        className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 ring-1 outline-none focus:none "
+                        type="email" placeholder="insertemail@gmail.com"/>
+                </label>
+                <label className="block mb-5">
+                    <span className="text-gray-500">Comment</span>
+                    <textarea className="border py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:none ring-1"  placeholder="" rows={8}/>
+                </label>
+            </form>
         </main>
     );
 }
