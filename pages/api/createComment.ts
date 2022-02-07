9f5c6342c8489af59ba236a54fdedfa9747855fb
Heytemisan <temyicy@gmail.com>
2022-02-07 23:43:00 +0100

    feat: code cleanUp

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -9,9 +9,24 @@ const config = {
 };
 const client = sanityClient(config);
 
-export default function createComment(
+export default async function createComment(
 req: NextApiRequest,
 res: NextApiResponse
 ) {
-res.status(200).json({ name: 'John Doe' })
+    const { _id, name, email, comment } = JSON.parse(req.body);
+    try {
+        await client.create({
+            _type: 'comment',
+            post: {
+                _type: 'reference',
+                _ref: _id
+            },
+            name,
+            email,
+            comment
+        });
+    } catch (err) {
+        return res.status(500).json({message: `Couldn't submit comment`, err})
+    }
+    res.status(200).json({ name: 'Comment submitted' });
 }
